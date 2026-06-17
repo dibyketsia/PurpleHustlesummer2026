8f4a79f73a6318cf7c6091bfccccb994817a2997
Ketsia Diby <ketsia@192.168.1.45>
2026-06-17 18:41:47 +0000

    week 2 lab2

--- profile.py
+++ profile.py
@@ -1,1 +1,19 @@
-full_name= "Ketsia Diby"
+full_name = "Ketsia Diby"
+nickname = "Ketsia"
+age = 15
+used_python_before = False
+hobbies = ["writing", "art","coding"]
+favorites = {
+    "movie": "The Hunger Games",
+    "food": "pasta",
+    "hobby": "writing"
+}
+
+
+print(full_name)
+print(nickname)
+print(age)
+print(nickname, age)
+print(hobbies)
+print(favorites)
+print(used_python_before)
