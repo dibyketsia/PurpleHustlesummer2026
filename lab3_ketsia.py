7ec56c7dce63d71113e56758ea10e8559553ea43
Ketsia Diby <ketsia@192.168.1.53>
2026-06-29 17:43:13 +0000

    lab3_ketsia

---
+++ lab3_ketsia.py
@@ -0,0 +1,32 @@
+# Ketsia | Lab 3 | Intro to hustle
+
+# Ticket 1
+
+username = "ketsiahustle"
+
+print(len(username))
+# 12
+# yes len() counted every character
+
+
+# Ticket 2
+
+print(username[0])
+print(username[len(username)-1])
+# letter k and e printed
+# becaus the number minus 1 is the index number
+
+
+# Ticket 3
+
+print("Welcome to Loop, " + username)
+print(f"Welcome to Loop, {username}")
+# both lines look identical on the string
+# the concatention was easier to use
+
+# Ticket 4
+
+username[0] = "X" # run this, it breaks on purpose
+print(username[KETSIAHUSTLE])
+feed = [hustle, summer, coding]
+print(len(feed))
